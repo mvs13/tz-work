95048bd88557b600394747c438c5c78c2fa0add2
Max V. Shumakov <shumakovmv@gmail.com>
2023-06-03 11:54:16 +0300

    tzWork zero state app

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          <div>Title</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  setup () {
+    return {
+    }
+  }
+})
+</script>
